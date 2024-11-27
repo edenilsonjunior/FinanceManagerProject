5e71839e72fa4df8aa3d5fa0829f42e5370b84a9
Henrique Bispo <henriquefbispo.dev@gmail.com>
2024-11-27 20:40:04 -0300

    feat: add Filter

---
+++ FinanceManager/src/main/java/br/edu/ifsp/arq/tsi/arqweb2/financeManager/filters/AuthFilter.java
@@ -0,0 +1,30 @@
+package br.edu.ifsp.arq.tsi.arqweb2.financeManager.filters;
+
+import br.edu.ifsp.arq.tsi.arqweb2.financeManager.model.entity.user.User;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/index.jsp"}, filterName = "Auth")
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        HttpSession session = httpRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp");
+        } else {
+            chain.doFilter(request, response);
+        }
+
+    }
+}
