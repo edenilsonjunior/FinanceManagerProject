0ad7979cda8c601ec151245c1a1c3dbc4d25e1da
Edenilson Junior <edenilsonju10@gmail.com>
2025-01-17 04:03:53 -0300

    adiciona metodo auxiliar

--- FinanceManager/src/main/java/br/edu/ifsp/arq/tsi/arqweb2/financeManager/utils/Utils.java
+++ FinanceManager/src/main/java/br/edu/ifsp/arq/tsi/arqweb2/financeManager/utils/Utils.java
@@ -1,9 +1,13 @@
 package br.edu.ifsp.arq.tsi.arqweb2.financeManager.utils;
 
 import br.edu.ifsp.arq.tsi.arqweb2.financeManager.model.entity.user.User;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class Utils {
+
+    private static Gson gson = new Gson();
 
     public static User getUser(HttpServletRequest request) {
 
@@ -17,5 +21,13 @@ public class Utils {
     public static Boolean isLoggedIn(HttpServletRequest request) {
         var session = request.getSession(false);
         return (session != null && session.getAttribute("customer") != null);
+    }
+
+    public static JsonObject createResponse(String title, Object value) {
+        var response = new JsonObject();
+        var jsonValue = gson.toJson(value);
+
+        response.addProperty(title, jsonValue);
+        return response;
     }
 }
